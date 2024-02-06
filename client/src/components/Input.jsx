23ad39b5418b11003b4710c4882086b21f899236
singhalgunjan5 <78017706+singhalgunjan5@users.noreply.github.com>
2024-02-07 02:11:20 +0530

    backend created and merged with the frontend3

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -4,19 +4,27 @@ import TextField from "@mui/material/TextField";
 import { Box } from "@mui/material";
 import { useState } from "react";
 import Button from '@mui/material/Button';
-
+import axios from 'axios';
 
 
 export default function Input({ addTodo }) {
+
     const [input,setInput] = useState("")
+
+    const backendUrl = 'http://localhost:3000';
+    
     function onchange(e)
     {
-   
+      
       setInput(e.target.value);
     }
-    function handleSubmit()
+    async function handleSubmit()
     {
-        addTodo(input);
+     //  alert(`${backendUrl}/api/todo`)
+     
+        const response = await axios.post(`${backendUrl}/api/todo`, { text: input });
+        console.log('Todo created:', response.data);
+        addTodo( { text: input });
         setInput("");
     }
   return (
--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Input from "./Input";
-import { useState } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import DeleteIcon from "@mui/icons-material/Delete";
-
 import IconButton from "@mui/material/IconButton";
+import axios from 'axios';
+
+
 
 export default function TodoList() {
-  const [todos, setTodos] = useState([]);
 
-  function addTodo(todo) {
-    setTodos([...todos, todo]);
+  const [todos, setTodos] = useState([]);
+  const backendUrl = 'http://localhost:3000';
+  useEffect(() => {
+    async function fetchTodos() {
+      try {
+        const response = await axios.get(`${backendUrl}/api/todo`);
+        setTodos(response.data);
+      } catch (error) {
+        console.error('Error fetching todos:', error);
+      }
+    }
+    fetchTodos();
+  }, [todos]);
+
+ 
+
+  function addTodo(newTodo) {
+   
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+    
   }
-  function handledelete(index)
+
+  async function handledelete(id)
   {
-    const updatedTodos = [...todos];
-    updatedTodos.splice(index, 1);
-    setTodos(updatedTodos);
+   // alert(id);
+    setTodos(prevTodos => prevTodos.filter(todo => todo._id !== id));
+    await axios.delete(`${backendUrl}/api/todos/${id}`);
+     
   }
   return (
     <>
@@ -30,10 +50,10 @@ export default function TodoList() {
         {todos.map((todo, index) => (
           <ListItem alignItems="flex-start" key={index}>
             <ListItemText
-              primary={todo}
+              primary={todo.text}
               color="primary"
             />
-           <ListItemAvatar onClick={() => handledelete(index)}>
+           <ListItemAvatar onClick={() => handledelete(todo._id)}>
 
             <Avatar>
               <DeleteIcon />
@@ -47,3 +67,4 @@ export default function TodoList() {
     </>
   );
 }
+
